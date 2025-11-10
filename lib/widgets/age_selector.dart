0c78bf123e643558c509f8c4a77f01c2e604ae8d
Harshit <84241825+harshit2k4@users.noreply.github.com>
2025-11-11 00:18:21 +0530

    component: create age selector widget

---
+++ lib/widgets/age_selector.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:yabc/widgets/secondary_button.dart';
+
+class AgeSelector extends StatelessWidget {
+  const AgeSelector({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        color: Theme.of(context).colorScheme.primaryContainer,
+      ),
+      height: 200,
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Age",
+                style: TextStyle(
+                  fontSize: 18,
+                  color: Theme.of(context).colorScheme.onSecondaryContainer,
+                ),
+              ),
+            ],
+          ),
+          // show current selected weight
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "20",
+                style: TextStyle(
+                  fontSize: 60,
+                  fontWeight: FontWeight.bold,
+                  color: Theme.of(context).colorScheme.onSecondaryContainer,
+                ),
+              ),
+            ],
+          ),
+          // increase button
+          Expanded(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                SecondaryButton(onPress: () {}, icon: Icons.add),
+                SecondaryButton(onPress: () {}, icon: Icons.remove),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
