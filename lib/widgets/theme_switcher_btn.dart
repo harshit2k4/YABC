89eb456a1c744f88397d734d44e472336f7c734a
Harshit <84241825+harshit2k4@users.noreply.github.com>
2025-11-12 14:58:43 +0530

    feat: add theme toggle

--- lib/widgets/theme_switcher_btn.dart
+++ lib/widgets/theme_switcher_btn.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:yabc/controllers/theme_controller.dart';
 
 class ThemeSwitcherBtn extends StatelessWidget {
   const ThemeSwitcherBtn({super.key});
 
   @override
   Widget build(BuildContext context) {
+    ThemeController themeController = Get.put(ThemeController());
+
     return Row(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
@@ -17,8 +21,36 @@ class ThemeSwitcherBtn extends StatelessWidget {
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-              IconButton(onPressed: () {}, icon: Icon(Icons.dark_mode)),
-              IconButton(onPressed: () {}, icon: Icon(Icons.light_mode)),
+              IconButton(
+                onPressed: () {
+                  if (themeController.isDark.value) {
+                    // switch theme to light
+                    themeController.changeTheme();
+                  }
+                },
+                icon: Icon(
+                  Icons.light_mode,
+                  // set color of active theme to primary color
+                  color: themeController.isDark.value
+                      ? Theme.of(context).colorScheme.onSecondaryContainer
+                      : Theme.of(context).colorScheme.primary,
+                ),
+              ),
+              IconButton(
+                onPressed: () {
+                  if (!themeController.isDark.value) {
+                    // switch theme to dark
+                    themeController.changeTheme();
+                  }
+                },
+                icon: Icon(
+                  Icons.dark_mode,
+                  // set color of active theme to primary color
+                  color: !themeController.isDark.value
+                      ? Theme.of(context).colorScheme.onSecondaryContainer
+                      : Theme.of(context).colorScheme.primary,
+                ),
+              ),
             ],
           ),
         ),
